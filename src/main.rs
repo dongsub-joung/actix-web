c1a608cd03609f9b0e5835838cb3d8aad123d6ab
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2025-01-10 22:46:36 +0900

    service

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,17 @@ use actix_web::HttpServer;
 async fn main() -> std::io::Result<()>{
     HttpServer::new(|| {
         actix_web::App::new()
-            .route("/", actix_web::web::get().to(index))
+            .service(
+                actix_web::web::scope("/v1")
+                    .route("/profile", actix_web::web::get().to(index))
+                    .route("/profile", actix_web::web::post().to(insert))
+            )
+            .service(
+                actix_web::web::scope("/v2")
+                    .route("/whatever", actix_web::web::get().to(index))
+                    .route("/profile", actix_web::web::post().to(insert))
+            )
+
     })
     .bind(("127.0.0.1", 8001))?
     .run()
@@ -13,4 +23,8 @@ async fn main() -> std::io::Result<()>{
 
 async fn index() -> &'static str{
     "hello world"
+}
+
+async fn insert() -> &'static str{
+    "inserted"
 }
