69c9c4d0861ca6c35757ac00c5397731b06cb137
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2025-01-12 18:54:14 +0900

    study

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,41 @@
-use actix_web::HttpServer;
+use std::sync::Mutex;
+
+use actix_web::{web, HttpServer};
+
+struct Messenger{
+    message: String,
+}
+
+struct MutableState{
+    messenger: Mutex<Messenger>,
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()>{
-    HttpServer::new(|| {
-        actix_web::App::new()
-            .service(
-                actix_web::web::scope("/v1")
-                    .route("/profile", actix_web::web::get().to(index))
-                    .route("/profile", actix_web::web::post().to(insert))
-            )
-            .service(
-                actix_web::web::scope("/v2")
-                    .route("/whatever", actix_web::web::get().to(index))
-                    .route("/profile", actix_web::web::post().to(insert))
-            )
+    let app_data= actix_web::web::Data::new(MutableState{
+        messenger: Mutex::new(Messenger { message: "hello".to_string() })
+    });
 
+    // @todo "move" doc
+    // global mutable status
+    // + "Arc" type //smart pointer
+    HttpServer::new(move || {
+        actix_web::App::new()
+        .app_data(app_data.clone())
+        .route("/", web::post().to(update))
+        .route("/", web::post().to(get))
     })
     .bind(("127.0.0.1", 8001))?
     .run()
     .await
 }
 
-async fn index() -> &'static str{
-    "hello world"
+async fn update(app_date: actix_web::web::Data<MutableState>) -> String{
+    let mut messenger= app_date.messenger.lock().unwrap();
+    messenger.message= format!("{} world", messenger.message);
+    "".to_string()
 }
 
-async fn insert() -> &'static str{
-    "inserted"
+async fn get(app_date: actix_web::web::Data<MutableState>) -> String{
+    app_date.messenger.lock().unwrap().message.clone()
 }
