90bfb6f8b8e7768c0dc7eec651a9a20c9302f2b7
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2025-01-21 17:17:29 +0900

    1-4

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,78 @@
 use std::sync::Mutex;
+use serde::Deserialize;
 
-use actix_web::{web, HttpServer};
+use actix_web::{web::{self, Json, Path}, App, HttpServer};
 
-struct Messenger{
-    message: String,
+#[derive(Deserialize)]
+struct EntityId{
+    id: i64,
 }
 
-struct MutableState{
-    messenger: Mutex<Messenger>,
+#[derive(Clone)]
+struct FinalUser{
+    id: i64,
+    user_name: String,
+    full_name: String,
+}
+
+#[derive(Deserialize)]
+struct NewUser{
+    user_name: String,
+    full_name: String,
+}
+
+struct AppState{
+    users: std::sync::RwLock<Vec<FinalUser>>
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()>{
-    let app_data= actix_web::web::Data::new(MutableState{
-        messenger: Mutex::new(Messenger { message: "hello".to_string() })
-    });
+    let app_data= web::Data::new(
+        AppState{
+            users: std::sync::RwLock::new(vec![
+                FinalUser { id: 1, user_name: "sadf".to_string(), full_name: "sadffff".to_string()}, 
+                FinalUser { id: 1, user_name: "qwer".to_string(), full_name: "qwerrrrr".to_string()}, 
+                FinalUser { id: 1, user_name: "zxcv".to_string(), full_name: "zxcvvvvv".to_string()}, 
+            ])
+        }
+    );
 
-    // @todo "move" doc
-    // global mutable status
-    // + "Arc" type //smart pointer
     HttpServer::new(move || {
-        actix_web::App::new()
-        .app_data(app_data.clone())
-        .route("/", web::post().to(update))
-        .route("/", web::post().to(get))
+        App::new()
+            .app_data(app_data.clone())
+            .service(
+                web::scope("/v1")
+                    .service(
+                        web::resource("/user/{id}")
+                            .route(web::get().to(get_user_name))
+                    )
+                    .service(
+                        web::resource("/user")
+                            .route(web::post().to(insert_user))   
+                    )
+            )
     })
     .bind(("127.0.0.1", 8001))?
     .run()
     .await
 }
 
-async fn update(app_date: actix_web::web::Data<MutableState>) -> String{
-    let mut messenger= app_date.messenger.lock().unwrap();
-    messenger.message= format!("{} world", messenger.message);
-    "".to_string()
+async fn insert_user(app_data: web::Data<AppState>, new_user: Json<NewUser>)-> String{
+    let mut users= app_data.users.write().unwrap();
+    let max_id= users.iter().max_by_key(|usr| { usr.id }).unwrap().id;
+    users.push(FinalUser { id: max_id+1, user_name: new_user.user_name.clone() , full_name: new_user.full_name.clone() });
+
+    users.last().unwrap().id.to_string()
 }
 
-async fn get(app_date: actix_web::web::Data<MutableState>) -> String{
-    app_date.messenger.lock().unwrap().message.clone()
+async fn get_user_name(app_data: web::Data<AppState>, params: Path<EntityId>) -> String{
+    let users= app_data.users.read().unwrap();
+    users
+        .iter()
+        .find(|usr| {
+            usr.id == params.id
+        })
+        .unwrap()
+        .clone()
+        .user_name
 }
